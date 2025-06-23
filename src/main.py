e3e8ce002709c888e8c12de61fc9c071b97f7ce6
Feelipee1 <juanfelipe.martinez1@utp.edu.co>
2025-06-23 14:03:33 -0500

    Proyecto funcional con mosek para las instances , separado por archivos py, luego importados en main. Consideracion importante, se debe correr desde la terminal y colocar este comando:  python -m src.main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 """
 Codigo para ejecutar todo el proyecto de optimizacion
 """
+
 import json
+import os
+import sys
 from src.data.load_data import preparar_datos_desde_json
-from src.optimizer.model import construir_modelo_pyomo
+from src.optimizer.model.model import construir_modelo_pyomo
 from pyomo.environ import *
 from src.visualization.visualization import presentar_resultados
-import os
+from pyomo.solvers.plugins.solvers.mosek_direct import MOSEKDirect
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main():
     # Configuración inicial usando la librería os
-    ruta_instancia = os.path.join("data", "instance1.json")
-    
+    ruta_instancia = r"D:\Challenge_ASOCIO25\data\instance1.json"
     # Cargar y procesar datos
     with open(ruta_instancia, "r", encoding="utf-8") as f:
         datos = json.load(f)
@@ -20,9 +23,7 @@ def main():
     
     # Construir y resolver el modelo
     modelo = construir_modelo_pyomo(param)
-    mosek_dir = os.path.join("src", "optimizer", "solvers", "mosek")
-    mosek_path = os.path.abspath(os.path.join(mosek_dir, "mosek.exe"))
-    os.environ["PATH"] = mosek_dir + os.pathsep + os.environ.get("PATH", "")
+    mosek_path = r"D:\Challenge_ASOCIO25\src\optimizer\solvers\mosek\mosek.exe"
     solver = SolverFactory("mosek", executable=mosek_path)
     results = solver.solve(modelo, tee=True)
     
